f16a6e4e3f03160f17e9c9acfc54fd2a50cf270f
Diego <dtellez95@gmail.com>
2022-07-24 11:42:26 +0100

    define train function

--- snake-pygame/agent.py
+++ snake-pygame/agent.py
@@ -11,7 +11,12 @@ LR = 0.001 # learning rate
 class Agent:
     
     def __init__(self) -> None:
-        pass
+        self.n_games = 0
+        self.epsilon = 0 # controls randomnewss
+        self.gamma = 0 # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY) # popleft() callled when max mem is reached
+
+        # TODO: model, trainer
 
     def get_state(self, game):
         pass
@@ -22,14 +27,48 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def traing_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
         pass
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get the current state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform a move and get a new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train the short mem of the agent
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember all
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done: #game over
+            # train the long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # TODO: agent.model.save()
+                print('GAME: ', agent.n_games, 'SCORE: ', score, 'RECORD: ', record)
+
+                # TODO: plot data
 
 if __name__ == '__main__':
     train()
